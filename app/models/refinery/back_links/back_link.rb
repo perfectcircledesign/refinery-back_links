4036b73a0c856be56ebd44a37e78547f340d18e5
Gerard Mopp <gerardmopp@gmail.com>
2017-03-22 09:54:20 +0200

    adding roo gem

--- app/models/refinery/back_links/back_link.rb
+++ app/models/refinery/back_links/back_link.rb
@@ -35,9 +35,9 @@ module Refinery
 
       def self.open_spreadsheet(file)
         case File.extname(file.original_filename)
-          when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
-          when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-          when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+          when ".csv" then ::Roo::Csv.new(file.path, nil, :ignore)
+          when ".xls" then ::Roo::Excel.new(file.path, nil, :ignore)
+          when ".xlsx" then ::Roo::Excelx.new(file.path, nil, :ignore)
           else raise "Unknown file type: #{file.original_filename}"
         end
       end
--- refinerycms-back_links.gemspec
+++ refinerycms-back_links.gemspec
@@ -13,6 +13,7 @@ Gem::Specification.new do |s|
 
   # Runtime dependencies
   s.add_dependency             'refinerycms-core',    '~> 2.0.3'
+  s.add_dependency             'roo',  '~> 1.13.2'
 
   # Development dependencies (usually used for testing)
   s.add_development_dependency 'refinerycms-testing', '~> 2.0.3'
